c3c9208ec59536dc532d1fc8683984e58a8266f5
lorpheli <cyril.lorphelin@cc.in2p3.fr>
2017-04-18 14:17:12 +0200

    First commit

---
+++ src/main/java/org/emiage/module306/DemoClass.java
@@ -0,0 +1,302 @@
+/*
+ * Class pour le module 306
+ * Activité 1
+ *
+ */
+package org.emiage.module306;
+
+import java.util.Random;
+
+/**
+ *
+ * @author cyril
+ */
+public final class DemoClass {
+
+    /**
+     * Constructeur privee pour empecher l'instantiation de classe.
+     */
+    private DemoClass() {
+    }
+
+    /**
+     * Generation d'un tableau d'entiers aleatoires.
+     *
+     * @param size taille du tableau
+     * @return un tableau d'entiers aleatoires
+     */
+    public static int[] randomTab(final int size) {
+        Random rand = new Random();
+        int[] tab = new int[size];
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = rand.nextInt();
+        }
+        return tab;
+    }
+
+    /**
+     * Renvoie l'indice du plus grand élément du tableau.
+     *
+     * @param tab :: tableau dans lequel on effectue la recherche
+     * @param taille :: taille du tableau
+     * @return indiceMax :: indice le plus grand
+     *
+     * return int l'indice du plus grand élément*
+     */
+    private static int max(final int[] tab, final int taille) {
+
+        // on considère que le plus grand élément est le premier
+        int i = 0, indiceMax = 0;
+
+        while (i < taille) {
+            if (tab[i] > tab[indiceMax]) {
+                indiceMax = i;
+            }
+            i++;
+        }
+
+        return indiceMax;
+
+    }
+
+    /**
+     * Échange deux éléments d'un tableau.
+     *
+     *
+     * @param tab tableau d'entiers à trier
+     * @param x : indice du premier élément
+     * @param y : indice du deuxième élément return void
+     *
+     */
+    private static void echanger(final int[]  tab, final int x, final int y) {
+
+        int tmp;
+
+        tmp = tab[x];
+        tab[x] = tab[y];
+        tab[y] = tmp;
+
+    }
+
+    /**
+     * Tri a bulles.
+     *
+     * @param tab tableau d'entiers à trier
+     *
+     *
+     */
+    public static void bubbleSort(final int[] tab) {
+
+        boolean sorted = false;
+        int taille = tab.length;
+
+        if (tab == null || tab.length == 0) {
+            return;
+        }
+
+        while (!sorted) {
+
+            sorted = true;
+
+            for (int i = 0; i < taille - 1; i++) {
+
+                if (tab[i] > tab[i + 1]) {
+                    echanger(tab, i, i + 1);
+                    sorted = false;
+                }
+
+            }
+
+            taille--;
+
+        }
+
+    }
+
+    /**
+     * Tri par selection.
+     *
+     * @param tab tableau d'entiers à trier return void
+     *
+     */
+    public static void selectSort(final int[] tab) {
+        int indiceMax;
+        int taille = tab.length;
+
+        if (tab == null || tab.length == 0) {
+            return;
+        }
+
+        // à chaque tour de boucle, on va déplacer le plus grand élément
+        // vers la fin du tableau, on diminue donc à chaque fois sa taille
+        // car le dernier élément est obligatoirement correctement
+        // placé (et n'a donc plus besoin d'être parcouru/déplacé)
+        // tant qu'il reste des éléments non triés
+        for (; taille > 1; taille--) {
+            indiceMax = max(tab, taille);
+            echanger(tab, taille - 1, indiceMax); // on échange le dernier élément avec le plus grand
+        }
+    }
+
+    /**
+     * Tri rapide.
+     *
+     * @param tab tableau d'entiers à trier return void
+     */
+    public static void rapideSort(final int[] tab) {
+
+        if (tab == null || tab.length == 0) {
+            return;
+        }
+
+        int length = tab.length;
+        quickSort(tab, 0, length - 1);
+    }
+
+    /**
+     * Tri rapide de la sous partition.
+     *
+     * @param tab tableau d'entiers à trier
+     * @param lowerIndex indice de debut
+     * @param higherIndex indice de fin return void
+     */
+    private static void quickSort(final int[] tab, final int lowerIndex, final int higherIndex) {
+
+        int i = lowerIndex;
+        int j = higherIndex;
+        // calculate pivot number, I am taking pivot as middle index number
+        int pivot = tab[lowerIndex + (higherIndex - lowerIndex) / 2];
+        // Divide into two arrays
+        while (i <= j) {
+            /**
+             * In each iteration, we will identify a number from left side which
+             * is greater then the pivot value, and also we will identify a
+             * number from right side which is less then the pivot value. Once
+             * the search is done, then we exchange both numbers.
+             */
+            while (tab[i] < pivot) {
+                i++;
+            }
+            while (tab[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                echanger(tab, i, j);
+                //move index to next position on both sides
+                i++;
+                j--;
+            }
+        }
+        // call quickSort() method recursively
+        if (lowerIndex < j) {
+            quickSort(tab, lowerIndex, j);
+        }
+        if (i < higherIndex) {
+            quickSort(tab, i, higherIndex);
+        }
+    }
+
+    /**
+     * Tri fusion.
+     *
+     * @param tab tableau d'entiers à trier
+     *
+     *
+     */
+    public static void mergeSort(final int[] tab) {
+        int longueur = tab.length;
+        if (longueur > 0) {
+            mergeSortPartition(tab, 0, longueur - 1);
+        }
+    }
+
+    /**
+     * Tri fusion : paritionnement.
+     *
+     * @param tab tableau d'entiers à trier
+     * @param deb indice début
+     * @param fin indice fin
+     *
+     *
+     */
+
+    private static void mergeSortPartition(final int[] tab, final int deb, final int fin) {
+        if (deb != fin) {
+            int milieu = (fin + deb) / 2;
+            mergeSortPartition(tab, deb, milieu);
+            mergeSortPartition(tab, milieu + 1, fin);
+            merge(tab, deb, milieu, fin);
+        }
+    }
+
+      /**
+     * Tri fusion : fusion finale .
+     *
+     * @param tab tableau d'entiers à trier
+     * @param deb1 indice début initial
+     * @param fin1 indice fin initial
+     * @param fin2 indice fin   final
+     *
+     */
+
+    private static void merge(final int[] tab, final int deb1, final int fin1, final int fin2) {
+        int deb2 = fin1 + 1;
+
+        //on recopie les éléments du début du tableau
+        int[] table1  = new int[fin1 - deb1 + 1];
+        for (int i = deb1; i <= fin1; i++) {
+            table1[i - deb1] = tab[i];
+        }
+
+        int compt1 = deb1;
+        int compt2 = deb2;
+
+        for (int i = deb1; i <= fin2; i++) {
+            //c'est que tous les éléments du premier tableau ont été utilisés
+            //tous les éléments ont donc été classés
+            if (compt1 == deb2) { 
+                break; 
+            }            //c'est que tous les éléments du second tableau ont été utilisés
+            else if (compt2 == (fin2 + 1))  {
+                tab[i] = table1[compt1 - deb1]; //on ajoute les éléments restants du premier tableau
+                compt1++;
+            } else if (table1[compt1 - deb1] < tab[compt2]) {
+                tab[i] = table1[compt1 - deb1]; //on ajoute un élément du premier tableau
+                compt1++;
+            } else {
+                tab[i] = tab[compt2]; //on ajoute un élément du second tableau
+                compt2++;
+            }
+        }
+    }
+
+    /**
+     * Programme principal.
+     *
+     * @param args Arguments du programme: taille du tableau
+     */
+    public static void main(final String[] args) {
+        System.out.println("Generation du tableau");
+        int[] tab ;        
+        int size;
+        
+        if (args.length > 0){
+            size = Integer.valueOf(args[0]);        
+        }
+        else {             
+              System.out.println("tableau de paramètres vide");  
+             size = 100;
+        }        
+        
+         tab=randomTab(size);        
+         mergeSort(tab);
+         tab=randomTab(size); 
+         rapideSort(tab);
+         tab=randomTab(size); 
+         selectSort(tab);
+         tab=randomTab(size); 
+         bubbleSort(tab);
+         
+    }
+
+}
---
+++ src/test/java/org/emiage/module306/DemoClassTest.java
@@ -0,0 +1,225 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.emiage.module306;
+
+import junit.framework.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author cyril
+ */
+public class DemoClassTest extends TestCase {
+
+    @org.junit.BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.Before
+    @Override
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    @Override
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void testTabCreation() {
+        int[] tab1;
+        int size = 6;
+
+        tab1 = DemoClass.randomTab(size);
+
+        assertEquals(tab1.length, size);
+    }
+
+    /**
+     * Test of bubbleSort method, of class DemoClass.
+     */
+    @org.junit.Test
+    public void testBubbleSort() {
+        System.out.println("bubbleSort");
+        int[] tab1 = {2, 1, 0};
+        int[] tab2 = {0, 1, 2};
+
+        DemoClass.bubbleSort(tab1);
+        assertArrayEquals(tab2, tab1);
+
+    }
+
+    /**
+     * Test of selectSort method, of class DemoClass.
+     */
+    @org.junit.Test
+    public void testSelectSort() {
+        System.out.println("selectSort");
+
+        int[] tab1 = {2, 1, 0};
+        int[] tab2 = {0, 1, 2};
+
+        DemoClass.selectSort(tab1);
+        assertArrayEquals(tab2, tab1);
+    }
+
+    /**
+     * Test of rapideSort method, of class DemoClass.
+     */
+    @org.junit.Test
+    public void testQuickSort() {
+        System.out.println("rapideSort");
+
+        int[] tab1 = {2, 1, 0};
+        int[] tab2 = {0, 1, 2};
+
+        DemoClass.rapideSort(tab1);
+        assertArrayEquals(tab2, tab1);
+    }
+
+    /**
+     * Test of mergeSort method, of class DemoClass.
+     */
+    @org.junit.Test
+    public void testMergeSort() {
+        System.out.println("mergeSort");
+
+        int[] tab1 = {2, 1, 0};
+        int[] tab2 = {0, 1, 2};
+
+        DemoClass.mergeSort(tab1);
+        assertArrayEquals(tab2, tab1);
+    }
+
+    @org.junit.Test
+    public void testSortsEmptyArray() {
+
+        System.out.println("testSortsEmptyArray");
+
+        int[] a1 = new int[0];
+        int[] a2 = new int[0];
+
+        DemoClass.rapideSort(a1);
+        assertArrayEquals(a2, a1);
+
+        DemoClass.bubbleSort(a1);
+        assertArrayEquals(a2, a1);
+
+        DemoClass.mergeSort(a1);
+        assertArrayEquals(a2, a1);
+
+        DemoClass.selectSort(a1);
+        assertArrayEquals(a2, a1);
+
+    }
+
+    @org.junit.Test
+    public void testSortsArraySorted() {
+
+        System.out.println("testSortsArraySorted");
+
+        int[] a1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] a2 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        DemoClass.rapideSort(a1);
+        assertArrayEquals(a2, a1);
+
+        DemoClass.bubbleSort(a1);
+        assertArrayEquals(a2, a1);
+
+        DemoClass.mergeSort(a1);
+        assertArrayEquals(a2, a1);
+
+        DemoClass.selectSort(a1);
+        assertArrayEquals(a2, a1);
+
+    }
+
+    @org.junit.Test
+    public void testSortsArrayInverseSorted() {
+
+        System.out.println("testSortsArrayInverseSorted");
+
+        int[] a1 = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        int[] a2 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        DemoClass.rapideSort(a1);
+        assertArrayEquals(a2, a1);
+
+        DemoClass.bubbleSort(a1);
+        assertArrayEquals(a2, a1);
+
+        DemoClass.mergeSort(a1);
+        assertArrayEquals(a2, a1);
+
+        DemoClass.selectSort(a1);
+        assertArrayEquals(a2, a1);
+    }
+
+    @org.junit.Test
+    public void testSortsArrayUnSorted() {
+
+        System.out.println("testSortsArrayInverseSorted");
+
+        int[] a1 = {3, 2, 5, 6, 7, 10, 1, 4, 9, 8, 0};
+        int[] a2 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println("rapideSort");
+        DemoClass.rapideSort(a1);
+        assertArrayEquals(a2, a1);
+
+        System.out.println("bubbleSort");
+        DemoClass.bubbleSort(a1);
+        assertArrayEquals(a2, a1);
+
+        System.out.println("mergeSort");
+        DemoClass.mergeSort(a1);
+        assertArrayEquals(a2, a1);
+
+        System.out.println("selectSort");
+        DemoClass.selectSort(a1);
+        assertArrayEquals(a2, a1);
+    }
+
+    @org.junit.Test
+    public void testAnalysisMethods() {
+
+        int i;
+        int[] tab1;
+        long a, b , c ,d ,e ,f , g, h ;
+        int[] sizes = {100, 1000, 10000, 100000 , 200000};
+
+        for (i = 0; i < sizes.length; i++) {
+            tab1 = DemoClass.randomTab(sizes[i]);
+
+            a = System.currentTimeMillis();
+            DemoClass.rapideSort(tab1);
+            b = System.currentTimeMillis();
+            System.out.println("rapideSort with size " + sizes[i]  + " => " + (b - a) + " millisec" );
+
+            c = System.currentTimeMillis();
+            DemoClass.bubbleSort(tab1);
+            d = System.currentTimeMillis();
+            System.out.println("bubbleSort with size " + sizes[i]  + " => " + (d - c) + " millisec ");
+
+            e = System.currentTimeMillis();
+            DemoClass.mergeSort(tab1);
+            f = System.currentTimeMillis();
+            System.out.println("MergeSort with size " + sizes[i]  + " => " + (f - e) + " millisec");
+
+            g = System.currentTimeMillis();
+            DemoClass.selectSort(tab1);
+            h = System.currentTimeMillis();
+            System.out.println("selectSort with size " + sizes[i]  + " => " + ( h - g ) + " millisec");
+
+        }
+
+    }
+}
